0d42d514e809dd3df360bee45ed3d15138b28604
ninnypants <tyrel@ninnypants.com>
2012-06-24 22:10:30 -0600

    Added basic config

---
+++ config-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+// base url for the wiki
+define('URL', 'http://wiki-url.com');
+
+// directory constants
+define('ABSPATH', dirname(__FILE__));
+define('INC', ABSPATH.'/include');
+define('DOC', ABSPATH.'/pages');
