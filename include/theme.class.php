66b7a3a8be8ad4b3a1c87f57cc47fe87b6146151
ninnypants <tyrel@ninnypants.com>
2012-06-29 00:26:32 -0600

    added missing functions and added login form

--- include/theme.class.php
+++ include/theme.class.php
@@ -13,7 +13,16 @@ class Theme {
 	Find page template file to be included
 	*/
 	public function get_template(){
+		require THEME.'/index.php';
+	}
 
+	/*
+	Load page in to be handled
+	*/
+	public function load_page($action){
+		$this->file = new File(format_page_name($action->page, true));
+		$this->markdown = Markdown($this->file->data);
+		$this->action = $action->action;
 	}
 
 	/*
@@ -27,6 +36,25 @@ class Theme {
 	Display page content
 	*/
 	public function the_content(){
+		switch($this->action){
+			case 'login':
+				$this->login_form();
+			break;
+
+			case 'edit':
+				$this->edit_form();
+			break;
+
+			default:
+				echo $this->markdown;
+			break;
+		}
+	}
+
+	/*
+	Output title of the current page
+	*/
+	public function the_title(){
 
 	}
 
@@ -49,13 +77,6 @@ class Theme {
 	}
 
 	/*
-	Determine the current page
-	*/
-	public function get_page(){
-
-	}
-
-	/*
 	Load header template
 	*/
 	public function get_header(){
@@ -73,7 +94,17 @@ class Theme {
 	Output the login form
 	*/
 	public function login_form(){
-
+		?>
+		<form action="" method="post">
+			<fieldset>
+				<legend>Log In</legend>
+				<p><label for="username">Username: <input type="text" name="username" id="username"></label></p>
+				<p><label for="password">Password: <input type="password" name="password" id="password"></label></p>
+				<input type="hidden" name="login" value="1">
+				<input type="submit" value="Log In">
+			</fieldset>
+		</form>
+		<?php
 	}
 
 	/*
@@ -81,7 +112,7 @@ class Theme {
 	*/
 	public function edit_form(){
 		?>
-		<form action="<?php echo $this->get_base_url('/save/') ?>" method="post">
+		<form action="<?php echo get_base_url('/save/') ?>" method="post">
 			<fieldset>
 				<legend>Editing</legend>
 				<label for="text">Content:</label><br>
