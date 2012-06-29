7d91aaf2173c9eee28e44faf401581f7ddd8b776
ninnypants <tyrel@ninnypants.com>
2012-06-29 00:20:45 -0600

    Added class inits and login action. Also added resource registering function, and theme functions.php include

--- index.php
+++ index.php
@@ -8,11 +8,28 @@ require_once ABSPATH.'/users.php';
 
 // get classes
 require_once INC.'/user.class.php';
+$user = new User();
+
+// log in if form is being submitted
+if(isset($_POST['login'])){
+	$user = new User($_POST['username'], $_POST['password']);
+}
+
 require_once INC.'/resources.class.php';
+$resources = new Resources();
+
 require_once INC.'/theme.class.php';
+$theme = new Theme();
+
 require_once INC.'/file-system.class.php';
 require_once INC.'/markdown.class.php';
 require_once INC.'/markdown-wiki.class.php';
+require_once INC.'/functions.php';
+
+register_default_resources();
+
+if(file_exists(THEME.'/functions.php'))
+	include THEME.'/functions.php';
 
 $wiki = new MarkdownWiki();
 $wiki->handle_request();
